6a7aa626b695964ecf9388ff79b1d91c968b67a5
mika <mika36191@gmail.com>
2024-06-15 16:15:11 -0700

    aaaa
    aaa

---
+++ resources/lang/en.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'home' => 'Home',
+    'about' => 'About',
+    'services' => 'Services',
+    'mitra' => 'Partner',
+    'gallery' => 'Gallery',
+    'contact' => 'Contact',
+    // tambahkan terjemahan lainnya sesuai kebutuhan
+];
---
+++ resources/lang/id.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'home' => 'Beranda',
+    'about' => 'Tentang Kami',
+    'services' => 'Layanan',
+    'mitra' => 'Mitra',
+    'gallery' => 'Galeri',
+    'contact' => 'Kontak',
+    // tambahkan terjemahan lainnya sesuai kebutuhan
+];
